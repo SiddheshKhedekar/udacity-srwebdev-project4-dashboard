e7bd8404dafccdaa4069090ea399242b0742072e
Daniel Escobar <escobard90@gmail.com>
2017-01-24 23:59:59 -0700

    type: feat
    added map base component, ready to begin refactoring map component into application

--- src/components/app.js
+++ src/components/app.js
@@ -7,7 +7,7 @@ export default class App extends Component {
     return (
     	<div>
         <View />
-      	</div>
+      </div>
     );
   }
 }
---
+++ src/components/map.js
@@ -0,0 +1,18 @@
+// =============================================================
+// 
+// 	view.js
+//
+// =============================================================
+
+import React, { Component } from 'react';
+
+
+export default class Map extends Component {
+  render() {
+    return (
+    	<div>
+ 			<p>Test</p>	
+      	</div>
+    );
+  }
+}

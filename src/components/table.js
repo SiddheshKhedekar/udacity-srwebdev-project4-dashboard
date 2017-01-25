8b6f7fa5572b90c46a98a5ba471d19ca13d3f4c9
Daniel Escobar <escobard90@gmail.com>
2017-01-25 01:20:59 -0700

    type: feat
    updated correct semantics to show table widget within a card

--- src/components/table.js
+++ src/components/table.js
@@ -9,9 +9,9 @@ import React, { Component } from 'react';
 export default class Table extends Component {
   render() {
     return (
-        <div>
-<table id="example" className="display" width="100%"></table>
-        </div>
+        <article className="card">
+			<table id="example" className="display" width="100%"></table>
+        </article>
     );
   }
 }

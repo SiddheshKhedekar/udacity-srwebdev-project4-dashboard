584eef4da71f458406f800bb93416fad62de2c6e
Daniel Escobar <escobard90@gmail.com>
2017-01-24 22:25:29 -0700

    type: feat
    scrapped routing for simple application setup

--- src/index.js
+++ src/index.js
@@ -1,37 +1,15 @@
-
-
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { createStore, combineReducers } from 'redux'
-import { Provider } from 'react-redux'
-import { Router, Route, browserHistory } from 'react-router'
-import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/app';
-import Charts from './components/chart';
-import Table from './components/table';
 import reducers from './reducers';
 
-// Add the reducer to your store on the `routing` key
-const store = createStore(
-  combineReducers({
-    ...reducers,
-    routing: routerReducer
-  })
-)
-
-// Create an enhanced history that syncs navigation events with the store
-const history = syncHistoryWithStore(browserHistory, store)
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <Provider store={store}>
-    { /* Tell the Router to use our enhanced history */ }
-    <Router history={history}>
-      <Route path="/" component={App}>
-        <Route path="/chart" component={Charts}/>
-        <Route path="/table" component={Table}/>
-      </Route>
-    </Router>
-  </Provider>,
-  document.querySelector('#application')
-)
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('#application'));

ba723a7f1a51d309dad872aca0a99aa5fc74283d
Daniel Escobar <escobard90@gmail.com>
2017-01-26 02:14:54 -0700

    type: refactor
    removed dummy link content

--- src/containers/nav.jsx
+++ src/containers/nav.jsx
@@ -90,54 +90,6 @@ class Nav extends Component {
 			  
 			            <ul id="menu-content" className="menu-content collapse out">
 							{this.renderViewLinks()}
-			                <li  data-toggle="collapse" data-target="#products" className="collapsed active">
-			                  <a href="#"><i className="fa fa-gift fa-lg"></i></a>
-			                </li>
-			                <ul className="sub-menu collapse" id="products">
-			                    <li className="active"><a href="#">CSS3 Animation</a></li>
-			                    <li><a href="#">General</a></li>
-			                    <li><a href="#">Buttons</a></li>
-			                    <li><a href="#">Tabs & Accordions</a></li>
-			                    <li><a href="#">Typography</a></li>
-			                    <li><a href="#">FontAwesome</a></li>
-			                    <li><a href="#">Slider</a></li>
-			                    <li><a href="#">Panels</a></li>
-			                    <li><a href="#">Widgets</a></li>
-			                    <li><a href="#">Bootstrap Model</a></li>
-			                </ul>
-
-
-			                <li data-toggle="collapse" data-target="#service" className="collapsed">
-			                  <a href="#"><i className="fa fa-globe fa-lg"></i> Services <span className="arrow"></span></a>
-			                </li>  
-			                <ul className="sub-menu collapse" id="service">
-			                  <li>New Service 1</li>
-			                  <li>New Service 2</li>
-			                  <li>New Service 3</li>
-			                </ul>
-
-
-			                <li data-toggle="collapse" data-target="#new" className="collapsed">
-			                  <a href="#"><i className="fa fa-car fa-lg"></i> New <span className="arrow"></span></a>
-			                </li>
-			                <ul className="sub-menu collapse" id="new">
-			                  <li>New New 1</li>
-			                  <li>New New 2</li>
-			                  <li>New New 3</li>
-			                </ul>
-
-
-			                 <li>
-			                  <a href="#">
-			                  <i className="fa fa-user fa-lg"></i> Profile
-			                  </a>
-			                  </li>
-
-			                 <li>
-			                  <a href="#">
-			                  <i className="fa fa-users fa-lg"></i> Users
-			                  </a>
-			                </li>
 			            </ul>
 			     </div>
 			</div>
@@ -147,7 +99,7 @@ class Nav extends Component {
   }
 }
 // exactly how it sounds, it maps the state into the props method
-// whatever returns, will show up as this.props inside of BookList
+// whatever returns, will show up as this.props inside of Class
 // this is a built in function of React
 function mapStateToProps(state) {
 

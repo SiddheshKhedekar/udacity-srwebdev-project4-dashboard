474b2c69c1c39558650b4a8b77f02606437356ee
Daniel Escobar <escobard90@gmail.com>
2017-01-26 06:55:04 -0700

    type: feat
    added secondary function to handle display

--- src/components/render_functions.jsx
+++ src/components/render_functions.jsx
@@ -7,33 +7,26 @@
 // 
 // shared functions to manipulate view components
 
-export function listHide(containerHidden, containerShow){
+export default function listHide(containerHidden, containerShow){
 		
 		var hide = document.querySelector(containerHidden);
 		var show = document.querySelector(containerShow);
 
-		// handles animations when list is hiding
+		// handles animations when list is hsiding
 		hide.classList.add('fadeOut');
 		hide.classList.remove('fadeInUp');
 
 		// handlesviewHide classes
+		show.classList.remove('fadeOut');
 		show.classList.remove('hidden');
 		setTimeout(function(){
 			show.classList.add('fadeIn');
 		}, 1000);
 
 
-		// hides from the dom after animation is over
-		setTimeout(function(){
+
 			hide.classList.add('hidden');
-		}, 500);
+
 		
 };
-
-export function clearState(clearVariable){
-
-	var clean = document.querySelector(cleanVariable);
-
 	
-
-};
